ace1bbc8d2a70be8177499d44619ef527198485f
Mikhail-Kavzov <kavzovm@mail.ru>
2022-10-16 02:01:24 +0300

    move to common

---
+++ CollectionsProject/Common/Hash/MyGuid.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
+namespace CollectionsProject.Hash
+{
+    public static class MyGuid
+    {
+        public static string GetGuid()
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+        }
+    }
+}
