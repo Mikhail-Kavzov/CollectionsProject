768b406cad922ddf2bedb535c6a0142bc6d5a030
Mikhail-Kavzov <kavzovm@mail.ru>
2022-10-30 15:11:00 +0300

    add comment hub

---
+++ CollectionsProject/Hub/CommentHub.cs
@@ -0,0 +1,32 @@
+﻿using CollectionsProject.Models.UserModels;
+using CollectionsProject.Services.Interfaces;
+using CollectionsProject.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CollectionsProject
+{
+    public class CommentHub : Hub
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentHub(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public async Task SubscribeComment(string itemId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, itemId);
+        }
+
+        [Authorize]
+        public async Task CreateComment(CommentViewModel model)
+        {
+            var comment = await _commentService.CreateComment(model);
+            await Clients.Group(model.ItemId.ToString()).SendAsync("GetNewComment", comment);
+        }
+    }
+}
