152f9815b52d445f394f898566ad17b3961ab7fe
Mikhail-Kavzov <kavzovm@mail.ru>
2022-10-31 13:03:20 +0300

    refactor

--- CollectionsProject/Program.cs
+++ CollectionsProject/Program.cs
@@ -82,12 +82,9 @@ internal class Program
             }
         }
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
             app.UseExceptionHandler("/Home/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
-        }
 
         app.UseStatusCodePagesWithReExecute("/Error/PageNotFound");
         app.UseHttpsRedirection();
